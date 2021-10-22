379b2466f966a461a20237103f8a40fd85bfc096
Harrison Caldwell <h.cald16@gmail.com>
2021-10-22 11:31:50 -0300

    Completed new char controller

--- Assets/InputManager.cs
+++ Assets/InputManager.cs
@@ -1,21 +1,23 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class InputManager : MonoBehaviour
 {
 
     [SerializeField] Movement movement;
+    [SerializeField] MouseLook mouseLook;
 
     PlayerControls controls;
     PlayerControls.GroundMovementActions groundMovement;
 
     Vector2 horizontalInput;
     Vector2 mouseInput;
+    
 
-    float MouseX, MouseY;
-    float SensitivityX;
-    float SensitivityY;
+    
+
 
     private void Awake()
     {
@@ -26,20 +28,16 @@ public class InputManager : MonoBehaviour
         groundMovement.Jump.performed += _ => movement.OnJumpPressed();
         groundMovement.MouseX.performed += ctx => mouseInput.x = ctx.ReadValue<float>();
         groundMovement.MouseY.performed += ctx => mouseInput.y = ctx.ReadValue<float>();
-        
+
 
     }
 
-    public void RecieveInput(Vector2 mouseInput)
-    {
-        MouseX = mouseInput.x * SensitivityX;
-        MouseY = mouseInput.y * SensitivityY;
-    }
 
     private void Update()
     {
         movement.ReceiveInput(horizontalInput);
-        transform.Rotate(Vector3.up, MouseX * Time.deltaTime);
+        mouseLook.ReceiveInput(mouseInput);
+        
     }
 
     private void OnEnable()
--- Assets/MouseLook.cs
+++ Assets/MouseLook.cs
@@ -1,17 +1,29 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class MouseLook : MonoBehaviour
 
 {
-    [SerializeField] float sensitivityX = 8f;
-    [SerializeField] float sensitivityY = 0.5f;
+    [SerializeField] float sensitivityX = 3f;
+    [SerializeField] float sensitivityY = 3f;
     float MouseX, MouseY;
+
+    [SerializeField] Transform playerCamera;
+    [SerializeField] float xClamp = 85f;
+    float xRotation = 0f;
 
     public void Update()
     {
         transform.Rotate(Vector3.up, MouseX * Time.deltaTime);
+
+        xRotation -= MouseY;
+        xRotation = Mathf.Clamp(xRotation, -xClamp, xClamp);
+        Vector3 targetRotation = transform.eulerAngles;
+        targetRotation.x = xRotation;
+        playerCamera.eulerAngles = targetRotation;
+            
     }
 
     public void ReceiveInput (Vector2 mouseInput)
